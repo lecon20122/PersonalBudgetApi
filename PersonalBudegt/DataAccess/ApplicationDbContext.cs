63decf4414559a5e0174f2e8b8fe6ca0de9c27e6
lecon20122 <lecon20122@gmail.com>
2023-08-06 20:30:54 +0300

    Add project files.

---
+++ PersonalBudegt/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalBudget.Models;
+
+namespace PersonalBudget.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
+    {
+    }
+}
---
+++ PersonalBudegt/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonalBudget.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+
+    }
+}
